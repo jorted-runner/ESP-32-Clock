f793916e49e6a51b3536bd735c4b5055c60267a4
janZ <janzander96@gmail.com>
2025-05-21 12:56:08 +0800

    added api call and got the time working, also included an image of wiring

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,21 @@
 #include <Arduino.h>
 #include "Secrets.h"
 
+//these are all the time related constants
+//this is the link to the public api
+#define NTP_SERVER     "time.google.com"
+//this will define the timezone
+#define UTC_OFFSET     0
+//this will define whether or not there is daylight savings
+#define UTC_OFFSET_DST 0
+
+//here are all the functions declarations
+void printLocalTime();
+
 void setup() {
+  //this establishes a serial connection with the PC. This makes debugging a lot easier
   Serial.begin(115200);
+  //the delay here is to make sure the connection is established and no communication starts without the PC being ready for it
   delay(1000);
   
   // Flash the LED to indicate the system setup is running (Did this to stop myself from going crazy)
@@ -39,11 +52,35 @@ void setup() {
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 
+  //this gets the time from the API
+  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+
   // Turns off the LED after setup is complete
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-  Serial.println("Still on...");
+  printLocalTime();
   delay(1000);
+}
+
+//this function is just to check if the api call works correctly. In a later stage this would be replaced
+//with an output to the display!
+void printLocalTime() {
+  //this will get the current time
+  struct tm timeinfo;
+
+  //this will check if the time is returned in a correct format
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Connection Err");
+
+    //this line is simply for debugging purposes
+    Serial.println(&timeinfo);
+
+    return;
+  }
+
+  //this will print out the time correctly
+  Serial.println(&timeinfo, "%H:%M:%S");
+  Serial.println(&timeinfo, "%d/%m/%Y   %Z");
 }
