9c5c568e9da09c47d4df88fcb311c3ba5d261837
janZ <janzander96@gmail.com>
2025-05-23 07:24:19 +0800

    Re organized code for the api call

---
+++ include/TimeAPICall.h
@@ -0,0 +1,21 @@
+#ifndef TIME_API_CALL_H
+#define TIME_API_CALL_H
+
+//these are all the time related constants
+//this is the link to the public api
+#define NTP_SERVER "time.google.com"
+//this will define the timezone
+//the offset is in seconds (ie if your Timezone is UTC+1 you need to put 3600 here)
+int utcOffset = 0;
+//this will define whether or not there is daylight savings
+//again this offset is in seconds. During the summer put 3600 here, and 0 in the winter
+int dstOffset = 0;
+
+
+//here are all the functions declarations
+
+//this will print out the time which was stored after getting it back from the api. This should be replaced later with
+//a function that outputs the time correctly onto the screen.
+void printLocalTime();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,8 @@
 #include <Arduino.h>
 #include "WiFiManager.h"
+#include "TimeAPICall.h"
 
-//these are all the time related constants
-//this is the link to the public api
-#define NTP_SERVER     "time.google.com"
-//this will define the timezone
-#define UTC_OFFSET     0
-//this will define whether or not there is daylight savings
-#define UTC_OFFSET_DST 0
 
-//here are all the functions declarations
-void printLocalTime();
 
 void setup() {
   //this establishes a serial connection with the PC. This makes debugging a lot easier
@@ -22,31 +14,12 @@ void setup() {
   connectToWiFi();
 
   //this gets the time from the API
-  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+  //utcOffset is set to 0 at the start, it is there to set your timezone
+  //dstOffset is also set to 0, it defines whether or not there is daylight savings time observed
+  configTime(utcOffset, dstOffset, NTP_SERVER);
 }
 
 void loop() {
   printLocalTime();
   delay(1000);
-}
-
-//this function is just to check if the api call works correctly. In a later stage this would be replaced
-//with an output to the display!
-void printLocalTime() {
-  //this will get the current time
-  struct tm timeinfo;
-
-  //this will check if the time is returned in a correct format
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Connection Err");
-
-    //this line is simply for debugging purposes
-    Serial.println(&timeinfo);
-
-    return;
-  }
-
-  //this will print out the time correctly
-  Serial.println(&timeinfo, "%H:%M:%S");
-  Serial.println(&timeinfo, "%d/%m/%Y   %Z");
 }
