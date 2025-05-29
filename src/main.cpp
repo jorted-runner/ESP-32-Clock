4a90455ff9e418efa13093b8b7938f32971d1dfd
jorted-runner <jorted.python@gmail.com>
2025-05-29 10:25:39 -0500

    updating the wifi manager to have a timeout when trying to connect. Created function to check if wifi is connected and reconnect if the connection is down.

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@
 void setup() {
   //this establishes a serial connection with the PC. This makes debugging a lot easier
   Serial.begin(115200);
+
   //initialize the I2C connection with the display
   u8g2.begin();
+
   //the delay here is to make sure the connection is established and no communication starts without the PC being ready for it
   delay(1000);
   
@@ -24,11 +26,9 @@ void setup() {
 }
 
 void loop() {
-  //printLocalTime();
-  //delay(1000);
+  checkWiFiAndReconnect();
+
   prepareDisplay();
-
   sendTimeToDisplayBuffer(); 
-
   outputOnDisplay();
 }
