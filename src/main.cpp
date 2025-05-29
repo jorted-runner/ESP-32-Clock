406a8e2588476c3984df34c61769f04b56b9c8ae
jorted-runner <jorted.python@gmail.com>
2025-05-29 10:50:59 -0500

    using FreeRTOS to keep time in sync and monitor wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "OutputOnDisplay.h"
 #include <U8g2lib.h>
 
-
+void wifiAndTimeTask(void * parameter);
 
 void setup() {
   //this establishes a serial connection with the PC. This makes debugging a lot easier
@@ -23,12 +23,32 @@ void setup() {
   //utcOffset is set to 0 at the start, it is there to set your timezone
   //dstOffset is also set to 0, it defines whether or not there is daylight savings time observed
   configTime(utcOffset, dstOffset, NTP_SERVER);
+
+  // Create task for keeping clock in sync and manage wifi connection
+  // pinning it to core 0 to keep it run it in parallel to the main loop, which is run in core 1
+  xTaskCreatePinnedToCore(
+    wifiAndTimeTask,  // Task Function
+    "WiFiTimeTask",   // Task Name
+    4096,             // Stack size (in words, not bytes)
+    NULL,             // Task input
+    1,                // Priority Level
+    NULL,             // Task handle, which we dont need
+    0                 // Setting the task to run on Core 0
+  );
 }
 
 void loop() {
-  checkWiFiAndReconnect();
-
   prepareDisplay();
   sendTimeToDisplayBuffer(); 
   outputOnDisplay();
+}
+
+void wifiAndTimeTask(void * parameter) { // FreeRTOS allows passing parameter to the task when created. Even though we aren't passing any parameters, this must be included because FreeRTOS expects the signature
+  for (;;) { // forever loop, its the same as using while(true)
+    checkWiFiAndReconnect();
+    configTime(utcOffset, dstOffset, NTP_SERVER);
+
+    Serial.println("Time synced and WiFi checked");
+    vTaskDelay(10000 / portTICK_PERIOD_MS); // I have this set to wait 10 seconds for testing, but we can change it to update at any interval we want
+  }
 }
