e1aecd6fff8aad807d3afd663a88bb6fd97e19f2
jorted-runner <jorted.python@gmail.com>
2025-06-05 17:03:29 -0500

    adding clock display option menu option

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -9,7 +9,7 @@
 #include <string>
 #include <algorithm>
 
-std::vector<std::string> mainMenuOptions = {"Timezone", "Timer", "Exit"};
+std::vector<std::string> mainMenuOptions = {"Timezone", "Timer", "Clock Options","Exit"};
 std::vector<int> tzMenuOptions = {-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 std::vector<std::string> dstMenuOptions = {"F", "T"};
 
@@ -86,11 +86,16 @@ void handleSelection() {
             // create timer menu or start timer
             Serial.println(mainMenuOptions[MAIN_MENU_INDEX].c_str());
         } else if (MAIN_MENU_INDEX == 2) {
+            // clock display option 12 or 24 hour
+
+
+            
+        } else if (MAIN_MENU_INDEX == 3) {
             MAIN_MENU_INDEX = 0;
             MAIN_MENU_ENTERED = false;
             clearPreviousMenuText();
             createInitialMenu();
-        } 
+        }
     }
 
 }
