cffcc85c20c9d3747496c208aececf7b714186d7
jorted-runner <jorted.python@gmail.com>
2025-06-09 17:02:57 -0500

    removing extra timer vars

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -12,10 +12,6 @@
 #include <Arduino.h>
 
 // Establish Menu variables
-bool TIMER_RUNNING = false;
-unsigned long timerStartMillis = 0;
-unsigned long pausedElapsedTime = 0;
-
 std::vector<std::string> mainMenuOptions = {"Timezone", "Timer", "TimeFormat", "Exit"};
 std::vector<int> tzMenuOptions = {-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 std::vector<std::string> dstMenuOptions = {"F", "T"};
